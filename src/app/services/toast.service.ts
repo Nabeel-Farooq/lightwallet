1f324161a1d596f61e013793555f33b7e4abeb45
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-26 16:51:41 +0500

    Update toast.service.ts

--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,25 +1,37 @@
-import { Injectable } from '@angular/core'
-import { ToastController } from '@ionic/angular'
-import { TranslateService } from '@ngx-translate/core'
+import { Injectable } from "@angular/core";
+import { ToastController } from "@ionic/angular";
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ToastService {
-
   constructor(
     public toastController: ToastController,
-    private translate: TranslateService,
-  ) {
-  }
+    private translate: TranslateService
+  ) {}
 
   async simpleToast(message, duration = 2000) {
-    const translations = await this.translate.get([message]).toPromise()
-    const toast = await this.toastController.create({
-      message: translations[message],
-      duration,
-    })
-    toast.present()
-  }
+    try {
+      const translations = await this.translate
+        .get([message])
+        .toPromise();
 
+      const toast = await this.toastController.create({
+        message: translations?.[message] || message,
+        duration,
+      });
+
+      await toast.present();
+    } catch (error) {
+      console.error("Toast error:", error);
+
+      const toast = await this.toastController.create({
+        message,
+        duration,
+      });
+
+      await toast.present();
+    }
+  }
 }
